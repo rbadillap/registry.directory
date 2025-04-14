98d27c222bef26bcccbd04a144a5f3784f2236f7
Ronny Badilla <info@ronnybadilla.com>
2025-04-14 06:48:12 -0600

    fix: unnecessary gradients

--- app/(app)/og/route.tsx
+++ app/(app)/og/route.tsx
@@ -11,21 +11,12 @@ export async function GET() {
     return new ImageResponse(
         (
             <div 
-                tw="flex items-center justify-center w-full h-full"
+                tw="flex items-center justify-center w-full h-full bg-black"
                 style={{
                     fontFamily: 'Geist',
                     background: 'linear-gradient(to bottom right, #000000, #111111)',
                 }}
             >
-                {/* Dot pattern overlay */}
-                <div 
-                    tw="absolute inset-0 opacity-10"
-                    style={{
-                        backgroundImage: 'radial-gradient(circle at 2px 2px, #666 1px, transparent 0)',
-                        backgroundSize: '24px 24px',
-                    }}
-                />
-                
                 {/* Decorative borders */}
                 <div tw="flex border absolute border-stone-700/50 border-dashed inset-y-0 left-16 w-[1px]" />
                 <div tw="flex border absolute border-stone-700/50 border-dashed inset-y-0 right-16 w-[1px]" />
@@ -39,7 +30,7 @@ export async function GET() {
                     </h1>
                     <p tw="text-xl text-slate-300/90 mt-4">
                         the place where
-                        <span tw="pl-2 inline-flex items-center">
+                        <span tw="pl-2 flex items-center">
                             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
                                 <path d="M21.0001 12.4286L12.4287 21" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"></path>
                                 <path d="M19.2857 3L3 19.2857" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"></path>
