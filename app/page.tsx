70a40de66f4fd2ce630d7c0e9583172ead4a9346
Ronny Badilla <info@ronnybadilla.com>
2025-04-13 21:23:20 -0600

    feat: added metadata (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,34 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Registry Directory: Discover, preview, copy, and paste components",
+  title: "registry.directory - a collection of shadcn/ui registries",
   description:
-    "Discover, preview, copy, and paste components from the shadcn/ui registry.",
+    "The place where shadcn/ui registries live. Discover, Preview, Copy, and Paste components.",
+  openGraph: {
+    images: [
+      {
+        url: "/og",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: [
+      {
+        url: "/og",
+      }
+    ],
+  },
 };
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex items-center gap-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-folder-code-icon lucide-folder-code">
-        <path d="M10 10.5 8 13l2 2.5"/>
-        <path d="m14 10.5 2 2.5-2 2.5"/>
-        <path d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2z"/>
-        </svg>
         <h1 className="text-sm font-medium font-mono">
           registry<span className="text-muted-foreground">.directory</span>
+          {" "}
+          <span className="text-xs text-foreground rounded-md border bg-rose-700 px-1">beta</span>
         </h1>
       </div>
 
@@ -30,8 +42,20 @@ export default async function Home() {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
           className="inline-flex size-4 text-muted-foreground">
-            <path d="M21.0001 12.4286L12.4287 21" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"></path>
-            <path d="M19.2857 3L3 19.2857" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"></path>
+            <path 
+              d="M21.0001 12.4286L12.4287 21" 
+              stroke="currentColor" 
+              strokeWidth="3" 
+              strokeLinecap="round" 
+              strokeLinejoin="round"
+            ></path>
+            <path
+              d="M19.2857 3L3 19.2857" 
+              stroke="currentColor" 
+              strokeWidth="3" 
+              strokeLinecap="round" 
+              strokeLinejoin="round"
+            ></path>
         </svg>
         </span>
         <span className="ml-1 font-mono font-bold">shadcn/ui</span>
