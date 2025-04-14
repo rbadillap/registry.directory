41e406278d0d4bbdd26e780c30a9aaa14e774a8d
Ronny Badilla <info@ronnybadilla.com>
2025-04-13 19:15:15 -0600

    feat: added vercel analytics

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
+      <Analytics />
     </ThemeProvider>
   );
 }
