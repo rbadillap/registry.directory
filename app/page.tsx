b035462f881eec1237929cbd4dd5cdb820f3d440
Samuel Breznjak <samuelbreznjak35@gmail.com>
2025-05-04 09:22:58 +0200

    feat: added neobrutalism components to the list (#8)

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/og",
-      }
+      },
     ],
   },
 };
@@ -31,7 +31,8 @@ export default async function Home() {
     },
     {
       name: "Aceternity UI",
-      description: "Professional Next.js, Tailwind CSS and Framer Motion components.",
+      description:
+        "Professional Next.js, Tailwind CSS and Framer Motion components.",
       url: "https://ui.aceternity.com/",
     },
     {
@@ -56,7 +57,8 @@ export default async function Home() {
     },
     {
       name: "Shadcn Form Builder",
-      description: "Create forms with Shadcn, react-hook-form and zod within minutes.",
+      description:
+        "Create forms with Shadcn, react-hook-form and zod within minutes.",
       url: "https://shadcn-form.com/",
     },
     {
@@ -69,42 +71,48 @@ export default async function Home() {
       description: "Create your own design system",
       url: "https://www.styleglide.ai/",
     },
+    {
+      name: "Neobrutalism components",
+      description:
+        "A collection of neobrutalism-styled, shadcn/ui based components.",
+      url: "https://neobrutalism.dev/",
+    },
   ];
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex items-center gap-2">
         <h1 className="text-sm font-medium font-mono">
-          registry<span className="text-muted-foreground">.directory</span>
-          {" "}
-          <span className="text-xs text-foreground rounded-md border bg-rose-700 px-1">beta</span>
+          registry<span className="text-muted-foreground">.directory</span>{" "}
+          <span className="text-xs text-foreground rounded-md border bg-rose-700 px-1">
+            beta
+          </span>
         </h1>
       </div>
 
       <div className="text-sm mt-10 px-4 text-center font-mono">
-        <span className="text-muted-foreground">
-          discover, preview, copy{" "}
-        </span>
+        <span className="text-muted-foreground">discover, preview, copy </span>
         <span className="text-foreground">
-        <svg
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="inline-flex size-4 text-muted-foreground">
-            <path 
-              d="M21.0001 12.4286L12.4287 21" 
-              stroke="currentColor" 
-              strokeWidth="3" 
-              strokeLinecap="round" 
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="inline-flex size-4 text-muted-foreground"
+          >
+            <path
+              d="M21.0001 12.4286L12.4287 21"
+              stroke="currentColor"
+              strokeWidth="3"
+              strokeLinecap="round"
               strokeLinejoin="round"
             ></path>
             <path
-              d="M19.2857 3L3 19.2857" 
-              stroke="currentColor" 
-              strokeWidth="3" 
-              strokeLinecap="round" 
+              d="M19.2857 3L3 19.2857"
+              stroke="currentColor"
+              strokeWidth="3"
+              strokeLinecap="round"
               strokeLinejoin="round"
             ></path>
-        </svg>
+          </svg>
         </span>
         <span className="ml-1 font-mono font-bold">shadcn/ui</span>
         <span className="text-muted-foreground"> registries</span>
