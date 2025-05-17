eb2f971b3b84ee17ae02fb21b1eff8469a9088db
Ronny Badilla <rbadillap@gmail.com>
2025-05-17 12:40:03 -0600

    Update layout.tsx (#18)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 
 import "@workspace/ui/globals.css"
 import { Providers } from "@/components/providers"
